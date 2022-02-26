0dc79faac2e6f6692c3acd032df588a5aefa3b1f
louib <code@louib.net>
2022-02-26 14:24:19 -0500

    First version of withdraw

--- src/account.rs
+++ src/account.rs
@@ -7,8 +7,6 @@ pub struct Account {
 
     held: f64,
 
-    total: f64,
-
     locked: bool,
 }
 impl Account {
@@ -18,12 +16,20 @@ impl Account {
                 // TODO verify that this transaction was never processed?
                 self.available += tx.amount as f64;
             }
-            TransactionType::Withdrawal => {}
+            TransactionType::Withdrawal => {
+                if self.available < tx.amount as f64 {
+                    return Err("Could not process transaction: Insufficient amount.".to_string());
+                }
+                self.available -= tx.amount as f64;
+            }
             TransactionType::Dispute => {}
             TransactionType::Resolve => {}
             TransactionType::Chargeback => {}
         };
         Ok(())
+    }
+    pub fn get_total(&self) -> f64 {
+        self.available + self.held
     }
 }
 
@@ -32,12 +38,29 @@ mod tests {
     use super::*;
 
     #[test]
-    pub fn test_deposit_into_account() {
+    pub fn test_deposit() {
         let mut account = Account {
             client_id: 1,
             held: 0.0,
             available: 0.0,
-            total: 0.0,
+            locked: false,
+        };
+        let mut tx = Transaction {
+            client_id: 1,
+            transaction_id: 1,
+            r#type: TransactionType::Deposit,
+            amount: 100.0,
+        };
+        account.process_transaction(tx).unwrap();
+        assert_eq!(account.available, 100.0);
+    }
+
+    #[test]
+    pub fn test_withdraw() {
+        let mut account = Account {
+            client_id: 1,
+            held: 0.0,
+            available: 0.0,
             locked: false,
         };
         let mut tx = Transaction {
