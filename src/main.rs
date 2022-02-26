0b883a5280634caa3690c1f0a22711f0af7affca
louib <code@louib.net>
2022-02-25 21:39:58 -0500

    Handle TransactionType serialization

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,35 @@ enum TransactionType {
     Resolve,
     Chargeback,
 }
+impl TransactionType {
+    pub fn to_string(&self) -> String {
+        match &self {
+            TransactionType::Deposit => "deposit".to_string(),
+            TransactionType::Withdrawal => "withdrawal".to_string(),
+            TransactionType::Dispute => "dispute".to_string(),
+            TransactionType::Resolve => "resolve".to_string(),
+            TransactionType::Chargeback => "chargeback".to_string(),
+        }
+    }
+    pub fn from_string(transaction_type: &str) -> Result<TransactionType, String> {
+        if transaction_type == "deposit" {
+            return Ok(TransactionType::Deposit);
+        }
+        if transaction_type == "withdrawal" {
+            return Ok(TransactionType::Withdrawal);
+        }
+        if transaction_type == "dispute" {
+            return Ok(TransactionType::Dispute);
+        }
+        if transaction_type == "resolve" {
+            return Ok(TransactionType::Resolve);
+        }
+        if transaction_type == "chargeback" {
+            return Ok(TransactionType::Chargeback);
+        }
+        Err(format!("Invalid transaction type {}.", transaction_type))
+    }
+}
 
 fn main() {
     println!("Hello, world!");
